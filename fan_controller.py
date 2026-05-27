60e090717fe3063ca4330da19aa6248eb431fca7
Duke Buerano <dukebueranoo@gmail.com>
2026-05-27 10:00:00 +0800

    chapter 2: binigyan ko na ng private na buhay yung fan natin gamit ang constructor, hidden and secure na ang mga parts

--- fan_controller.py
+++ fan_controller.py
@@ -3,3 +3,10 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
+
+    # Part 2: Dito na papasok yung constructor. Sinunod ko yung strict encapsulation gamit yung double underscores (__) para hidden at safe yung data natin.
+    def __init__(self, fan_speed=SLOW, fan_radius=5.0, fan_color="blue", fan_power_status=False):
+        self.__speed = fan_speed
+        self.__radius = float(fan_radius)
+        self.__color = fan_color
+        self.__on = fan_power_status
